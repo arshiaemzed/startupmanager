7372bf664ab92d0f9900546743a15c9387202869
EmZeD <emzed.cfx@gmail.com>
2026-06-26 13:41:50 +0330

    Changing access token expiration to  7 day and fixing a error handeling bug

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,5 +1,5 @@
 function errorHandler(err, req, res, next) {
-  const statusCode = err.status || 500;
+  const statusCode = err.statusCode || 500;
   const message = err.message || "Internal server error";
 
   return res.status(statusCode).json({ message: message });
--- token_generate.js
+++ token_generate.js
@@ -1,10 +1,12 @@
 const jwt = require("jsonwebtoken");
 
+// added 7 day for debugging so i dont have to keep changing my tokens
+// change to 15 minutes before production **IMPORTANT**
 function generateAccessToken(user) {
   return jwt.sign(
     { id: user.id, email: user.email, password: user.password },
     "SECRET_1234",
-    { expiresIn: 60 * 24 * 30 },
+    { expiresIn: "7d" },
   );
 }
 
