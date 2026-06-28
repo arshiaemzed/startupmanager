fa483c044eaaf60d365932de5569ec0021a0a180
EmZeD <emzed.cfx@gmail.com>
2026-06-28 11:18:59 +0330

    adding auth service signup test

---
+++ tests/authServiceSignup.test.js
@@ -0,0 +1,20 @@
+const bcrypt = require("bcrypt");
+const authRepository = require("../repositories/authRepository");
+const authService = require("../services/authService");
+
+jest.mock("bcrypt");
+
+jest.mock("../repositories/authRepository");
+
+test("", async () => {
+  bcrypt.hash.mockResolvedValue("mypassword1234");
+
+  authRepository.createNewUser.mockResolvedValue({
+    email: "myemail@gmail.com",
+    password: "mypassword1234",
+  });
+
+  expect(
+    await authService.registerUser("myemail@gmail.com", "mypassword1234"),
+  ).toEqual({ email: "myemail@gmail.com", password: "mypassword1234" });
+});
