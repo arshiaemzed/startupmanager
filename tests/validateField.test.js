1685d053633cf49030f31c63c910fb5fecf22798
EmZeD <emzed.cfx@gmail.com>
2026-07-14 17:59:36 +0330

    refactor: deleting tests

--- tests/validateField.test.js
+++ tests/validateField.test.js
@@ -1,25 +1,3 @@
 const validateField = require("../utils/validateField");
 
-const res = {
-  statusCode: 200,
-
-  status: jest.fn(function (code) {
-    this.statusCode = code;
-    return this;
-  }),
-
-  json: jest.fn(function (data) {
-    console.log(data);
-  }),
-};
-
-test("return error if not valid", () => {
-  validateField(null, res, 400, "Missing param");
-
-  expect(res.status).toHaveBeenCalledWith(400);
-
-  expect(res.json).toHaveBeenCalledWith({
-    success: false,
-    error: { message: "Missing param", code: 400 },
-  });
-});
+test("if the field in request body isnt empty it is valid", () => {});
