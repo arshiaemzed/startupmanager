bbe423d84ab638dc521b2539ef5fa63b59640fe7
EmZeD <emzed.cfx@gmail.com>
2026-08-05 11:12:15 +0330

    fix: refresh tokens byte identical issue  if generated in the same second

--- token_generate.js
+++ token_generate.js
@@ -1,4 +1,5 @@
 const jwt = require("jsonwebtoken");
+const uuid = require("uuid");
 
 function generateAccessToken(user) {
   return jwt.sign(
@@ -10,7 +11,8 @@ function generateAccessToken(user) {
 
 function genereateRefreshToken(user) {
   return jwt.sign(
-    { id: user.id, email: user.email },
+    { id: user.id, email: user.email, jti: uuid.v4() },
+
     process.env.JWT_REFRESH_TOKEN_SECRET,
     {
       expiresIn: "7d",
